1cf555594c8b6c9017cccb99a47a3856a3501930
Tristan Chen <96955025+yerenmao@users.noreply.github.com>
2024-12-08 22:44:13 +0800

    fix: crawler bugs (#12)

--- run.sh
+++ run.sh
@@ -12,15 +12,21 @@ docker-compose build
 docker-compose up -d
 
 if [[ "$new" = "new" ]]; then
+    path="crawler/pickle/"
+    prefix="NTU_"
+    files=('Admin' 'Course' 'Department' 'Professor' 'Student' 'Syllabus')
+    for file in "${files[@]}"; do
+        if [[ ! -e "${path}${prefix}${file}.pkl" ]]; then
+            unzip "${path}${prefix}${file}.pkl.zip" -d "${path}"
+        fi
+    done
     if [[ -d "crawler/.venv" ]]; then
-        echo ".venv exist"
         source crawler/.venv/bin/activate
     else
-        echo ".venv not exist"
         python3 -m venv crawler/.venv
         source crawler/.venv/bin/activate
         pip3 install -r crawler/requirements.txt
     fi
-    python3 crawler/setup.py
+    python3 crawler/main.py
     echo "crawler finished"
 fi
