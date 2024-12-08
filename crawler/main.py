ef7c5b7a70907be7ac6d2591c30d8daa642919b5
Tristan Chen <tristanchen92@gmail.com>
2024-12-08 22:43:13 +0800

    fix: crawler bugs

--- crawler/main.py
+++ crawler/main.py
@@ -16,22 +16,27 @@ DB_CONFIG = {
 
 path = os.getcwd()
 
-DIR = 'pickle'
-ADMIN_PKL = f'{path}/crawler/{DIR}/NTU_Admin.pkl'
-COURSE_PKL = f'{path}/crawler/{DIR}/NTU_Coures.pkl'
-DEPERTMENT_PKL = f'{path}/crawler/{DIR}/NTU_Department.pkl'
-PROFESSOR_PKL = f'{path}/crawler/{DIR}/NTU_Professor.pkl'
-STUDENT_PKL = f'{path}/crawler/{DIR}/NTU_Students.pkl'
-SYLLABUS_PKL = f'{path}/crawler/{DIR}/NTU_Syllabus.pkl'
+DIR = "pickle"
+ADMIN_PKL = f"{path}/crawler/{DIR}/NTU_Admin.pkl"
+COURSE_PKL = f"{path}/crawler/{DIR}/NTU_Course.pkl"
+DEPERTMENT_PKL = f"{path}/crawler/{DIR}/NTU_Department.pkl"
+PROFESSOR_PKL = f"{path}/crawler/{DIR}/NTU_Professor.pkl"
+STUDENT_PKL = f"{path}/crawler/{DIR}/NTU_Student.pkl"
+SYLLABUS_PKL = f"{path}/crawler/{DIR}/NTU_Syllabus.pkl"
+
 
 def execution_time(func):
     def wrapper(*args, **kwargs):
         start_time = time.time()
         result = func(*args, **kwargs)
         end_time = time.time()
-        print(f"Execution time for {func.__name__}: {end_time - start_time:.4f} seconds")
+        print(
+            f"Execution time for {func.__name__}: {end_time - start_time:.4f} seconds"
+        )
         return result
+
     return wrapper
+
 
 @execution_time
 def init():
@@ -42,13 +47,14 @@ def init():
             cursor = conn.cursor()
             connected = True
         except Exception as e:
-            pass # print(f"ERROR: {e}")
+            pass  # print(f"ERROR: {e}")
     time.sleep(1)
     return conn, cursor
 
+
 @execution_time
 def department(conn, cursor):
-    with open(DEPERTMENT_PKL, 'rb') as f:
+    with open(DEPERTMENT_PKL, "rb") as f:
         df = pickle.load(f)
     departments = [tuple(row) for row in df.to_records(index=False)]
     for d in departments:
@@ -62,54 +68,55 @@ def department(conn, cursor):
 
 @execution_time
 def admin():
-    with open(ADMIN_PKL, 'rb') as f:
+    with open(ADMIN_PKL, "rb") as f:
         df = pickle.load(f)
     data = []
     for _, row in df.iterrows():
-        data.append({
-            "id": row["id"],
-            "name": row["部門名稱"],
-            "email": row["email"],
-            "passwd": row["密碼"],
-            "role": row["身份"],
-            "department_id": row["科系代碼"]
-        })
-    body = {
-        "bulk": data
-    }
+        data.append(
+            {
+                "id": row["id"],
+                "name": row["部門名稱"],
+                "email": row["email"],
+                "passwd": row["密碼"],
+                "role": row["身份"],
+                "department_id": row["科系代碼"],
+            }
+        )
+    body = {"bulk": data}
     response = requests.post("http://localhost:8080/api/create/bulkuser", json=body)
     if response.status_code != 200:
         raise Exception("professor failed.")
-    
+
 
 @execution_time
 def professor():
-    with open(PROFESSOR_PKL, 'rb') as f:
+    with open(PROFESSOR_PKL, "rb") as f:
         df = pickle.load(f)
     data = []
     for _, row in df.iterrows():
-        data.append({
-            "id": row["id"],
-            "name": row["姓名"],
-            "email": row["E-Mail地址"],
-            "passwd": row["密碼"],
-            "role": row["身份"],
-            "department_id": row["dptname"]
-        })
-    body = {
-        "bulk": data
-    }
+        data.append(
+            {
+                "id": row["id"],
+                "name": row["姓名"],
+                "email": row["E-Mail地址"],
+                "passwd": row["密碼"],
+                "role": row["身份"],
+                "department_id": row["dptname"],
+            }
+        )
+    body = {"bulk": data}
     response = requests.post("http://localhost:8080/api/create/bulkuser", json=body)
     if response.status_code != 200:
         raise Exception("professor failed.")
 
-@execution_time  
+
+@execution_time
 def student(conn, cursor):
-    with open(STUDENT_PKL, 'rb') as f:
+    with open(STUDENT_PKL, "rb") as f:
         df = pickle.load(f)
     df = df[["student_id", "姓名", "email", "身份", "dept_code"]]
     result_tuples = df.apply(tuple, axis=1).tolist()
-    
+
     for r in result_tuples:
         insert_query = """
             INSERT INTO "user" (id, name, email, role, department_id)
@@ -118,20 +125,23 @@ def student(conn, cursor):
         cursor.execute(insert_query, r)
     conn.commit()
 
+
 @execution_time
 def course(conn, cursor):
-    with open(COURSE_PKL, 'rb') as f:
+    with open(COURSE_PKL, "rb") as f:
         course_df = pickle.load(f)
-    
-    with open(DEPERTMENT_PKL, 'rb') as f:
+
+    with open(DEPERTMENT_PKL, "rb") as f:
         dept_df = pickle.load(f)
 
-    merged_df = pd.merge(course_df, dept_df, left_on="授課對象", right_on='部門名稱',  how="left")
-    unique_df = merged_df.drop_duplicates(subset=['課號'], keep='first').copy()
-    unique_df['學分'] = unique_df['學分'].astype(float).astype(int)
-    filtered_df = unique_df[['課號', '課程名稱', '學分', '必/選修', '科系代碼']]
+    merged_df = pd.merge(
+        course_df, dept_df, left_on="授課對象", right_on="部門名稱", how="left"
+    )
+    unique_df = merged_df.drop_duplicates(subset=["課號"], keep="first").copy()
+    unique_df["學分"] = unique_df["學分"].astype(float).astype(int)
+    filtered_df = unique_df[["課號", "課程名稱", "學分", "必/選修", "科系代碼"]]
     result_tuples = filtered_df.apply(tuple, axis=1).tolist()
-    
+
     for r in result_tuples:
         insert_query = """
             INSERT INTO "course" (id, name, credits, type, department_id)
@@ -140,23 +150,24 @@ def course(conn, cursor):
         cursor.execute(insert_query, r)
     conn.commit()
 
+
 @execution_time
 def section(conn, cursor):
-    with open(COURSE_PKL, 'rb') as f:
+    with open(COURSE_PKL, "rb") as f:
         section_df = pickle.load(f)
 
-    with open(PROFESSOR_PKL, 'rb') as f:
+    with open(PROFESSOR_PKL, "rb") as f:
         prof_df = pickle.load(f)
 
     def parse_time(time_str):
-        chr2num = {'A': 10, 'B': 11, 'C': 12}
+        chr2num = {"A": 10, "B": 11, "C": 12}
         if not time_str:
             return {}
         result = {}
         current_key = None
         for part in time_str:
             # 如果是中文字符，作為 key
-            if '\u4e00' <= part <= '\u9fff':  
+            if "\u4e00" <= part <= "\u9fff":
                 current_key = part
                 if current_key not in result:
                     result[current_key] = []
@@ -167,12 +178,18 @@ def section(conn, cursor):
             elif part in chr2num:
                 result[current_key].append(chr2num[part])
         return result
-    
-    merged_df = pd.merge(section_df, prof_df, how='left', left_on='授課教師', right_on='姓名')
-    merged_df = merged_df[['流水號', '學年-學期', '時間', '教室', '總人數', '課號', 'id']]
-    merged_df['總人數'] = merged_df['總人數'].astype(int)
-    merged_df['時間'] = merged_df['時間'].apply(parse_time)
-    merged_df['時間'] = merged_df['時間'].apply(lambda x: json.dumps(x, ensure_ascii=False))
+
+    merged_df = pd.merge(
+        section_df, prof_df, how="left", left_on="授課教師", right_on="姓名"
+    )
+    merged_df = merged_df[
+        ["流水號", "學年-學期", "時間", "教室", "總人數", "課號", "id"]
+    ]
+    merged_df["總人數"] = merged_df["總人數"].astype(int)
+    merged_df["時間"] = merged_df["時間"].apply(parse_time)
+    merged_df["時間"] = merged_df["時間"].apply(
+        lambda x: json.dumps(x, ensure_ascii=False)
+    )
     result_tuples = merged_df.apply(tuple, axis=1).tolist()
     for r in result_tuples:
         insert_query = """
@@ -182,34 +199,61 @@ def section(conn, cursor):
         cursor.execute(insert_query, r)
     conn.commit()
 
+
 @execution_time
 def syllabus(conn, cursor):
-    with open(SYLLABUS_PKL, 'rb') as f:
+    with open(SYLLABUS_PKL, "rb") as f:
         syllabus_df = pickle.load(f)
 
-    with open(COURSE_PKL, 'rb') as f:
+    with open(COURSE_PKL, "rb") as f:
         course_df = pickle.load(f)
 
-    with open(PROFESSOR_PKL, 'rb') as f:
+    with open(PROFESSOR_PKL, "rb") as f:
         professor_df = pickle.load(f)
 
-    num_list = course_df['流水號2.0'].drop_duplicates().to_list()
-    professor_df = professor_df[['姓名', 'id']]
+    num_list = course_df["流水號2.0"].drop_duplicates().to_list()
+    professor_df = professor_df[["姓名", "id"]]
     filtered_df = syllabus_df[syllabus_df["流水號"].isin(num_list)]
-    merged_df = pd.merge(filtered_df, professor_df, how='left', left_on='授課教師', right_on='姓名')
-    selected_columns = ['課程概述', '課程目標', '課程要求', '預期每週課後學習時數', 'Office Hours', '指定閱讀', '參考書目', 'id', '流水號']
+    merged_df = pd.merge(
+        filtered_df, professor_df, how="left", left_on="授課教師", right_on="姓名"
+    )
+    selected_columns = [
+        "課程概述",
+        "課程目標",
+        "課程要求",
+        "預期每週課後學習時數",
+        "Office Hours",
+        "指定閱讀",
+        "參考書目",
+        "id",
+        "流水號",
+    ]
     merged_df = merged_df[selected_columns]
-    merged_df = merged_df[~merged_df['id'].isna()]
-    
+    merged_df = merged_df[~merged_df["id"].isna()]
+
     query = """
         SELECT id, section_id, semester FROM "section";
     """
     cursor.execute(query)
     result = cursor.fetchall()
-    df = pd.DataFrame(result, columns=['ID', 'section_id', 'semester'])
-    df['section_id'] = df['semester'].str.replace('-', '') + df['section_id'].astype(str)
-    merged_df = pd.merge(merged_df, df, how='left', left_on='流水號', right_on='section_id')
-    selected_columns = ['課程概述', '課程目標', '課程要求', '預期每週課後學習時數', 'Office Hours', '指定閱讀', '參考書目', 'id', 'ID']
+    df = pd.DataFrame(result, columns=["ID", "section_id", "semester"])
+    df["section_id"] = df["semester"].str.replace("-", "") + df["section_id"].astype(
+        str
+    )
+    merged_df = pd.merge(
+        merged_df, df, how="left", left_on="流水號", right_on="section_id"
+    )
+    selected_columns = [
+        "課程概述",
+        "課程目標",
+        "課程要求",
+        "預期每週課後學習時數",
+        "Office Hours",
+        "指定閱讀",
+        "參考書目",
+        "id",
+        "ID",
+    ]
     final_df = merged_df[selected_columns]
     result_tuples = final_df.apply(tuple, axis=1).tolist()
 
@@ -220,6 +264,7 @@ def syllabus(conn, cursor):
         """
         cursor.execute(insert_query, r)
     conn.commit()
+
 
 if __name__ == "__main__":
     conn, cursor = init()
